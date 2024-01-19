b25ae4d5bb584cd0f35c0f61dfc70a06e1a3cb40
JTG425 <golonka.10@buckeyemail.osu.edu>
2024-01-18 21:49:42 -0500

    Major Updates

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React, { useState, useRef } from "react";
+import { motion, AnimatePresence, useMotionValue } from "framer-motion";
+import { button } from "leva";
 
 const containerVariants = {
     hidden: {
         opacity: 0,
+        zIndex: -2,
+        transition: {
+            duration: 2,
+            when: "afterChildren"
+        }
     },
     shown: {
         opacity: 1,
         transition: {
             duration: 0.25,
             staggerChildren: 0.25,
+            when: "beforeChildren",
         }
     }
 }
@@ -17,14 +24,52 @@ const containerVariants = {
 const backgroundVariants = {
     hidden: {
         opacity: 0,
+        zIndex: -2,
+        transition: {
+            duration: 5,
+        }
     },
     shown: {
         opacity: 1,
         transition: {
             duration: 5,
         }
-    }
+    },
+
 }
+
+//https://codesandbox.io/p/sandbox/framer-motion-mouse-position-2b4sd?file=%2Fsrc%2FApp.js%3A40%2C1-69%2C2
+function getRelativeCoordinates(event, referenceElement) {
+    const position = {
+        x: event.pageX,
+        y: event.pageY
+    };
+
+    const offset = {
+        left: referenceElement.offsetLeft,
+        top: referenceElement.offsetTop,
+        width: referenceElement.clientWidth,
+        height: referenceElement.clientHeight
+    };
+
+    let reference = referenceElement.offsetParent;
+
+    while (reference) {
+        offset.left += reference.offsetLeft;
+        offset.top += reference.offsetTop;
+        reference = reference.offsetParent;
+    }
+
+    return {
+        x: position.x - offset.left,
+        y: position.y - offset.top,
+        width: offset.width,
+        height: offset.height,
+        centerX: (position.x - offset.left - offset.width / 2) / (offset.width / 2),
+        centerY: (position.y - offset.top - offset.height / 2) / (offset.height / 2)
+    };
+}
+
 
 const menuItem = {
     hidden: {
@@ -32,19 +77,43 @@ const menuItem = {
     },
     shown: {
         opacity: 1,
+        scale: 1.1,
         transition: {
-            duration: 0.5,
-        }
-    }
+            type: "spring",
+            duration: 2,
+        },
+    },
 }
+
 
 
 
 export default function Dropdown({ home, about, contact, projects, resume, setHome, setAbout, setContact, setProjects, setResume, handleButtonClick }) {
     const [toggle, setToggle] = useState(false);
     const [page, setPage] = useState("Home");
+    const [i, setI] = useState(0);
     const menuOptions = ["home", "about", "resume", "projects", "contact"]
     const displayed = ["Home", "About Me", "Resume", "My Projects", "Contact Me"]
+    const [mousePosition, setMousePosition] = useState({});
+    const buttonRef0 = useRef();
+    const buttonRef1 = useRef();
+    const buttonRef2 = useRef();
+    const buttonRef3 = useRef();
+    const buttonRef4 = useRef();
+
+    const handleMouseMove = e => {
+        if (i === 0) {
+            setMousePosition(getRelativeCoordinates(e, buttonRef0.current));
+        } else if (i === 1) {
+            setMousePosition(getRelativeCoordinates(e, buttonRef1.current));
+        } else if (i === 2) {
+            setMousePosition(getRelativeCoordinates(e, buttonRef2.current));
+        } else if (i === 3) {
+            setMousePosition(getRelativeCoordinates(e, buttonRef3.current));
+        } else if (i === 4) {
+            setMousePosition(getRelativeCoordinates(e, buttonRef4.current));
+        }
+    };
 
     const handleDropdownClick = (page, i) => {
         return () => {
@@ -82,8 +151,15 @@ export default function Dropdown({ home, about, contact, projects, resume, setHo
                             <motion.div variants={menuItem}>
                                 <motion.button
                                     className="dropdown-item"
+                                    ref={index === 0 ? buttonRef0 : index === 1 ? buttonRef1 : index === 2 ? buttonRef2 : index === 3 ? buttonRef3 : buttonRef4}
                                     key={index}
-                                    whileHover={{ scale: 1.1 }}
+                                    initial={{ scale: 0.9 }}
+                                    onHoverStart={() => setI(index)}
+                                    onMouseMove={e => handleMouseMove(e)}
+                                    whileHover={{
+                                        scale: 0.95,
+                                        background: `radial-gradient(circle at ${mousePosition.centerX * 100}% ${mousePosition.centerY * 100}%, #1a148c 0%, #2b2b2b 100%)`,
+                                    }}
                                     whileTap={{ scale: 0.9 }}
                                     onClick={handleDropdownClick(option, index)}
                                 >
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TypeAnimation } from 'react-type-animation';
+import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 
 function Home() {
+  const [show, setShow] = useState(true);
+
+  const stackVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.25,
+        when: "beforeChildren",
+      }
+    }
+  }
+
+  const stackItems = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+    }
+  }
+
+
+
+
 
   return (
     <div className="Home">
@@ -17,7 +48,6 @@ function Home() {
         speed={50}
         repeat={1}
       />
-
       <TypeAnimation
         className='type-animation'
         sequence={[
@@ -27,8 +57,10 @@ function Home() {
         wrapper='h2'
         cursor={false}
         speed={50}
-        repeat={1}
-      />
+        repeat={0}
+        onComplete={() => setShow(true)}
+      >
+      </TypeAnimation>
     </div>
 
   )
