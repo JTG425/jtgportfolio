16d1df29815297c75514f616b7c4899ec4e764da
JTG425 <golonka.10@buckeyemail.osu.edu>
2024-01-25 11:06:51 -0500

    sf

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react'
 import { TypeAnimation } from 'react-type-animation';
 import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
 import { motion, AnimatePresence } from 'framer-motion';
+import '../styles/home.css'
 
 
 
 function Home() {
   const [show, setShow] = useState(true);
+
+  const stackLogos = [
+    'https://i.imgur.com/FsACRhX.png', // React.js
+    'https://i.imgur.com/vlOBr4X.png', // Framer Motion
+    'https://i.imgur.com/jNNE26t.png', // Node.js
+    'https://i.imgur.com/u3y6ijb.png', // Three.js
+  ]
 
   const contentVariants = {
     hidden: {
@@ -25,7 +33,8 @@ function Home() {
 
 
   return (
-    <div className="Home">
+    <motion.div
+      className="Home">
       <TypeAnimation
         className='type-animation'
         sequence={[
@@ -50,9 +59,25 @@ function Home() {
         onComplete={() => setShow(true)}
       >
       </TypeAnimation>
-      <p>Tech Stack</p>
-    </div>
+      <motion.div
+        className='tech-stack'
+      >
+        {stackLogos.map((logo, index) => {
+          return (
+            <motion.img
+              key={index}
+              src={logo}
+              alt='tech-stack'
+              className='tech-stack-logo'
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            />
+          )
+        })}
 
+
+      </motion.div>
+    </motion.div>
   )
 }
 
