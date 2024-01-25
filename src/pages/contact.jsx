e8509332c21f9963d7a4775472b3630df68f3f53
JTG425 <77809745+JTG425@users.noreply.github.com>
2024-01-25 13:43:24 -0500

    jhf

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,12 +1,118 @@
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useForm, ValidationError } from '@formspree/react';
+import '../styles/contact.css'
+
 
 
 function Contact() {
+  const [state, handleSubmit] = useForm("xeqyelkj");
+  if (state.succeeded) {
+    return <p>Submitted!</p>;
+  }
+  const socialLinks = [
+    'https://www.instagram.com/joshuagolonka25/', // Instagram
+  ]
+
+  const contentVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+      }
+    }
+  }
+
+  const parentstackVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+      transition: {
+        delay: 5,
+        when: 'beforeChildren',
+        staggerChildren: 0.25,
+        duration: 1,
+      }
+    }
+  }
+
+  const stackItemsVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+      }
+    }
+  }
 
   return (
-    <div className="Home">
-        <h1>Let's Get In Touch</h1>
-    </div>
+    <motion.div className="Home">
+      <h1>Let's Get In Touch</h1>
+      <AnimatePresence>
+        <motion.div
+          className='contact-content'
+          initial={'hidden'}
+          animate={'shown'}
+          variants={parentstackVariants}
+        >
+          <motion.form
+            className='contact-form'
+            action='https://formspree.io/f/xeqyelkj'
+            method='POST'
+            onSubmit={handleSubmit}
+            variants={stackItemsVariants}
+          >
+            <motion.span
+              className='contact-info'
+            >
+              <motion.input
+                placeholder="Your Name"
+                id="Name"
+                type="text"
+                name="Name"
+              />
+              <motion.input
+                placeholder="Your Email"
+                id="email"
+                type="email"
+                name="email"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+            </motion.span>
+            <motion.textarea
+              placeholder="Your Message"
+              id="message"
+              name="message"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <motion.button
+              type="submit"
+              disabled={state.submitting}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Submit
+            </motion.button>
+          </motion.form>
+        </motion.div>
+      </AnimatePresence >
+    </motion.div>
 
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
 import { TypeAnimation } from 'react-type-animation';
-import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
 import { motion, AnimatePresence } from 'framer-motion';
 import '../styles/home.css'
 
 
 
 function Home() {
-  const [show, setShow] = useState(true);
-
   const stackLogos = [
     'https://i.imgur.com/FsACRhX.png', // React.js
     'https://i.imgur.com/vlOBr4X.png', // Framer Motion
     'https://i.imgur.com/jNNE26t.png', // Node.js
     'https://i.imgur.com/u3y6ijb.png', // Three.js
+    'https://i.imgur.com/ylyoerF.png', // HTML5
+    'https://i.imgur.com/XFReta1.png', // CSS3
+    'https://i.imgur.com/QurGWvg.png', // Talwind CSS
+
   ]
 
   const contentVariants = {
@@ -28,11 +29,37 @@ function Home() {
     }
   }
 
+  const parentstackVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+      transition: {
+        delay: 6,
+        when: 'beforeChildren',
+        staggerChildren: 0.25,
+        duration: 1,
+      }
+    }
+  }
 
+  const stackItemsVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    shown: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+      }
+    }
+  }
 
 
 
   return (
+
     <motion.div
       className="Home">
       <TypeAnimation
@@ -55,29 +82,32 @@ function Home() {
         wrapper='h2'
         cursor={false}
         speed={50}
-        repeat={0}
-        onComplete={() => setShow(true)}
+        repeat={1}
       >
       </TypeAnimation>
-      <motion.div
-        className='tech-stack'
-      >
-        {stackLogos.map((logo, index) => {
-          return (
-            <motion.img
-              key={index}
-              src={logo}
-              alt='tech-stack'
-              className='tech-stack-logo'
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            />
-          )
-        })}
-
-
-      </motion.div>
-    </motion.div>
+      <AnimatePresence>
+          <motion.div
+            className='tech-stack'
+            initial={'hidden'}
+            animate={'shown'}
+            variants={parentstackVariants}
+          >
+            {stackLogos.map((logo, index) => {
+              return (
+                <motion.img
+                  key={index}
+                  src={logo}
+                  variants={stackItemsVariants}
+                  alt='tech-stack'
+                  className='tech-stack-logo'
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                />
+              )
+            })}
+          </motion.div>
+      </AnimatePresence >
+    </motion.div >
   )
 }
 
